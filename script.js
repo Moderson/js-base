ede4c7e5d3a6d5553030fce4f485e0eb4a470779
Moderson <romanbabarevich@gmail.ru>
2023-09-18 15:50:21 +0700

    выполнил практику 1

---
+++ script.js
@@ -0,0 +1,18 @@
+//1 задача
+console.log("JavaScript")
+console.log("Flutter")
+console.log("TypeScript")
+
+//2 задача
+console.log("Значение 1 выражения")
+console.log(42 / 7)
+console.log("Значение 2 выражения")
+console.log(2** 4 )
+console.log("Значение 3 выражения")
+console.log(100 % 4)
+console.log("Сумма всех 3х выражений")
+console.log(6+16+0)
+
+//3 задача
+console.log(2**4 + 2/3)
+console.log("Ты просто супер! Правда!")
