3fc84344542b9f2933d14430f42b940231fba86d
Moderson <romanbabarevich@gmail.com>
2023-09-20 18:03:10 +0700

    сделал 3 задачу

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-//quest 1
-console.error('1задание')
+console.error('1 задание')
 console.log("Существует достаточно много экранирующих последовательностей.\
 Например последовательность \"\\n\" делает перевод на новую строку!")
-console.error('2задание')
-//quest 2
+
+console.error('2 задание')
 let a = 4
+console.log('стороны a,b,c= 4')
 let b = 4
 let c = 4
-console.log('периметр треугольника')
+console.log('P=a+b+c')
+console.log('Исходя из этого периметр треугольника')
 console.log(a+b+c)
-console.error('3задание')
-//quest 3
 
+console.error('3 задание')
 let text1 = 'Привет,мир!'
 console.log(text1)
-console.log(toUpperCase(text1))
-
+console.log('Используя toUpperCase делаем из исходного текста такой')
+console.log('Привет,Мир!'.toUpperCase())
