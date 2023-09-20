3d746053347ebe554490fea18d1e190002fbd513
Moderson <romanbabarevich@gmail.ru>
2023-09-20 16:14:17 +0700

    немного недоделал

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //quest 1
+console.error('1задание')
+console.log("Существует достаточно много экранирующих последовательностей.\
+Например последовательность \"\\n\" делает перевод на новую строку!")
+console.error('2задание')
+//quest 2
+let a = 4
+let b = 4
+let c = 4
+console.log('периметр треугольника')
+console.log(a+b+c)
+console.error('3задание')
+//quest 3
+
+let text1 = 'Привет,мир!'
+console.log(text1)
+console.log(toUpperCase(text1))
+
