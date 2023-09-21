2476b4c5c372a54614d8abce5555d1aa36134623
Moderson <romanbabarevich@gmail.com>
2023-09-21 20:35:16 +0700

    немного доделал 4 задачу

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ console.log('Используя toUpperCase делаем из исходного
 console.log('Привет,Мир!'.toUpperCase())
 
 console.error('Задание 4')
+console.log('Решение Задачи 4:')
+
+console.log('const answer1 = prompt(\'Введи число\')')
+
+console.log('if (answer1 > 0) (alert("Это положительное число"))')
+
+console.log('if (answer1 < 0) (alert("Это отрицательное число"))')
+
+console.log('if (answer1 == 0) (alert("Это 0"))')
+
 const answer1 = prompt('Введи число')
 if (answer1 > 0) (alert("Это положительное число"))
 
