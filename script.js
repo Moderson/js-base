763c6661bcbcc0b188a7168015a4e185a7de5097
Moderson <romanbabarevich@gmail.ru>
2023-09-27 15:21:21 +0700

    Начал делать практику

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-console.error('1 задание')
-console.log("Существует достаточно много экранирующих последовательностей.\
-Например последовательность \"\\n\" делает перевод на новую строку!")
-
-console.error('2 задание')
-let a = 4
-console.log('стороны a,b,c= 4')
-let b = 4
-let c = 4
-console.log('P=a+b+c')
-console.log('Исходя из этого периметр треугольника равен')
-console.log(a+b+c)
-
-console.error('3 задание')
-let text1 = 'Привет,мир!'
-console.log(text1)
-console.log('Используя toUpperCase делаем из исходного текста такой')
-console.log('Привет,Мир!'.toUpperCase())
-
-console.error('Задание 4')
+console.error('Задание 1')
 console.log('Решение Задачи 4:')
 
 console.log('const answer1 = prompt(\'Введи число\')')
@@ -28,28 +9,5 @@ console.log('if (answer1 < 0) (alert("Это отрицательное числ
 
 console.log('if (answer1 == 0) (alert("Это 0"))')
 
-const answer1 = prompt('Введи число')
-if (answer1 > 0) (alert("Это положительное число"))
-
-if (answer1 < 0) (alert("Это отрицательное число"))
-
-if (answer1 == 0) (alert("Это 0"))
-
-console.error('Задание 5')
-let answer0 = prompt('Введите место')
-function findCoupNumber(seatNumber) {
-    // Проверяем, что номер места находится в диапазоне от 1 до 
-    if (seatNumber >= 1 && seatNumber <= 99) {
-      // Вычисляем номер купе
-      var coupNumber = Math.ceil(seatNumber / 4);
-      return coupNumber;
-    } else {
-      return "Неверный номер места";
-    }
-  }
-  
-  // Пример использования функции
-  var seatNumber = answer0;
-  var coupNumber = findCoupNumber(seatNumber);
-  alert("Номер купе: " + coupNumber);
-
+console.error('Задание 2')
+let age 
