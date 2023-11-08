e80de51bec6cc314501e20f0f81885ed6c885bc6
Moderson <romanbabarevich@gmail.ru>
2023-11-08 16:02:09 +0700

    начал изучать тему 5

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-// Получаем объект div c классом lesson
-const lessonBlock = document.querySelector('.lesson')
-// И полностью удаляем его
-lessonBlock.remove()
+// Получаем элемент
+const element = document.querySelector('.lesson__item-list_pink')
+
+// Добавить класс (добавит рядом к уже существующим)
+element.classList.add('active')
+// Удалить класс
+element.classList.remove('active')
+// Добавить класс, если его нет, а если есть удалить.
+element.classList.toggle('active')
+// Проверка наличия класса, возбращает true/false.
+element.classList.contains('active')
