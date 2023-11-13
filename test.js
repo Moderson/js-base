5f4d73dd13f7fdca376926b74656667eabf27c15
Moderson <romanbabarevich@gmail.ru>
2023-11-13 15:52:38 +0700

    почти завершил  изучение

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
-function setScrollBy(value) {
-    // Прокрутить окно на координату value
-    window.scrollBy(0, value)
-    const windowScrollTop = window.scrollY
-    console.log(windowScrollTop)
-  }
-  
-  const input = document.querySelector('.lesson__input')
-  const btn = document.querySelector('.lesson__btn')
-  
-  // Забегая вперед
-  // Таким образом можно повешать прослушку события клика
-  btn.addEventListener(
-    'click',
-    () => setScrollBy(input.value)
-  )
+function setScrollTo(value) {
+	window.scrollTo(0, value)
+	const windowScrollTop = window.scrollY
+	console.log(windowScrollTop)
+}
+
+function setScrollToOptions(value) {
+	window.scrollTo({
+		top: value,
+		left: 0,
+		behavior: "smooth" // прокручивать страницу плавно
+
+		// ❗️ behavior: "smooth" теперь работает нормально в браузере Safari
+	})
+}
